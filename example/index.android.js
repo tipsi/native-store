c4ced9fe155ba5670758a130ff50574cf1bfecd5
dmitriy <dmitriy.malec@gmail.com>
2016-11-22 21:39:36 +0200

    - added simple four textView an default state for it

--- example/index.android.js
+++ example/index.android.js
@@ -13,20 +13,48 @@ import {
 } from 'react-native';
 
 export default class AwesomeProject extends Component {
+
+//  state = {
+//    js_js: 'Text for JS - JS',
+//    js_native: 'Text for JS - Native',
+//    native_js: 'Text for Native - JS',
+//    native_native: 'Text for Native - Native',
+//  }
+
+    state = {
+      js_js: 'None',
+      js_native: 'None',
+      native_js: 'None',
+      native_native: 'None',
+    }
+
   render() {
+
+  const state = this.state
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Data from Android Native Store
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
+        <Text style={styles.dataFile}>
+          JS - JS : {state.js_js}
         </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
+        <Text style={styles.dataFile}>
+          JS - Native : {state.js_native}
         </Text>
-      </View>
+        <Text style={styles.dataFile}>
+          Native - JS : {state.native_js}
+        </Text>
+        <Text style={styles.dataFile}>
+          Native - Native : {state.native_native}
+        </Text>
+        </View>
+//        <Text style={styles.instructions}>
+//          Double tap R on your keyboard to reload,{'\n'}
+//          Shake or press menu button for dev menu
+//        </Text>
+//      </View>
     );
   }
 }
@@ -39,7 +67,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 23,
+    color: '#000000',
     textAlign: 'center',
     margin: 10,
   },
@@ -48,6 +77,24 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+    dataFile: {
+      fontSize: 25,
+      textAlign: 'center',
+      color: '#008000',
+      marginTop: 25,
+      marginBottom: 25,
+      marginLeft: 10,
+      marginRight: 10,
+    },
+        dataFileTitle: {
+          fontSize: 25,
+          textAlign: 'center',
+          color: '#333333',
+          marginTop: 25,
+          marginBottom: 25,
+          marginLeft: 10,
+          marginRight: 10,
+        },
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
