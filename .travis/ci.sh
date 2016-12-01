889928782adbdc90d361c8b52f2fed0de5370d58
Anton Petrov <petrov.aap@gmail.com>
2016-12-01 22:12:16 +0300

    Added unit test command to travis script

--- .travis/ci.sh
+++ .travis/ci.sh
@@ -2,11 +2,13 @@
 
 case "${TRAVIS_OS_NAME}" in
   osx)
+    npm run test:ios
     cd example
     set -o pipefail && npm run build:ios | xcpretty -c -f `xcpretty-travis-formatter`
     npm run test:ios
   ;;
   linux)
+    npm run test:android
     cd example
     npm run build:android
     npm run test:android
