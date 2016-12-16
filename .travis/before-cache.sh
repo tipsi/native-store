6aeb4cee840dd3d6998c295b8e0dda75c46363e1
fAns1k <dmitriy.malec@gmail.com>
2016-12-16 20:34:28 +0200

    Issue with using command 'react-native link' for native-store has been solved. (#4)
    * - test commit
    
    * - fixes
    
    * - fixes in travice
    
    * - fixes in travice 2
    
    * - fixes in travice 3
    
    * - fixes in travice 4
    
    * - fixes in dummy data
    
    * - update readme
    
    * - update readme
    
    * Activate Android tests in local CI script
    
    * Reduce boilerplate code in index.{ios|android}.js
    
    * Updated README.md
    
    * - fix problems with uncorrect path to NativeStoreReactPackage after use command "react-native link native-store"
    
    * - clean travis cache and repeat
    
    * - try fix travis
    
    * - final fixes
    
    * Fixed cache issue in Travis

--- .travis/before-cache.sh
+++ .travis/before-cache.sh
@@ -2,10 +2,10 @@
 
 case "${TRAVIS_OS_NAME}" in
   osx)
-    rm -rf example/node_modules/native-module
+    rm -rf example/node_modules/native-store
   ;;
   linux)
-    rm -rf example/node_modules/native-module
+    rm -rf example/node_modules/native-store
     rm -f $HOME/.gradle/caches/modules-2/modules-2.lock
   ;;
 esac
