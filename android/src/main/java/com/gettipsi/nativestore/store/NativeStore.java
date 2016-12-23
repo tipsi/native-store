76f9728ab9b37bf48c997acb6d4c26087918cdf1
dmitriy <dmitriy.malec@gmail.com>
2016-12-23 22:28:52 +0200

    - first try. simple thread for setState

--- android/src/main/java/com/gettipsi/nativestore/store/NativeStore.java
+++ android/src/main/java/com/gettipsi/nativestore/store/NativeStore.java
@@ -12,60 +12,64 @@ import java.util.List;
  * Created by dmitriy on 11/22/16
  */
 public class NativeStore implements Observable {
-    private static NativeStore ourInstance;
-    private static HybridMap state;
-    private static List<Observer> observers;
+  private static NativeStore ourInstance;
+  private static HybridMap state;
+  private static List<Observer> observers;
 
 
-    public static NativeStore getInstance() {
-        if (ourInstance == null)
-            init();
-        return ourInstance;
-    }
+  public static NativeStore getInstance() {
+    if (ourInstance == null)
+      init();
+    return ourInstance;
+  }
 
-    private NativeStore() {
-        observers = new ArrayList<>();
-    }
+  private NativeStore() {
+    observers = new ArrayList<>();
+  }
 
-    private static void init() {
-        ourInstance = new NativeStore();
-    }
+  private static void init() {
+    ourInstance = new NativeStore();
+  }
 
-    @Override
-    public void subscribe(Observer o) {
-        observers.add(o);
-    }
+  @Override
+  public void subscribe(Observer o) {
+    observers.add(o);
+  }
 
-    @Override
-    public void unsubscribe(Observer o) {
-        observers.remove(o);
-    }
+  @Override
+  public void unsubscribe(Observer o) {
+    observers.remove(o);
+  }
 
-    @Override
-    public void notifyObservers() {
-        for (Observer observer : observers)
-            observer.update(state);
-    }
+  @Override
+  public void notifyObservers() {
+    for (Observer observer : observers)
+      observer.update(state);
+  }
 
-    public synchronized void setState(final HashMap<String, Object> value) {
-        state = new HybridMap(value);
-        notifyObservers();
-    }
+  public synchronized void setState(final HashMap<String, Object> value) {
+    state = new HybridMap(value);
+    notifyObservers();
+  }
 
-    public void setState(final ReadableMap value) {
+  public void setState(final ReadableMap value) {
+    new Thread(new Runnable() {
+      public void run() {
         setState(((ReadableNativeMap) value).toHashMap());
-    }
+      }
+    }).start();
+  }
 
-    public HybridMap getState() {
-        return state;
-    }
+  public HybridMap getState() {
+    return state;
+  }
 
-    public void close() {
-        if (ourInstance != null) {
-            state = null;
-            observers.clear();
-            observers = null;
-            ourInstance = null;
-        }
+  public void close() {
+    if (ourInstance != null) {
+      state = null;
+      observers.clear();
+      observers = null;
+      ourInstance = null;
     }
+  }
 }
