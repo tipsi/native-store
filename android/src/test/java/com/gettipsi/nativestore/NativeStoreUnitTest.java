d03b2caa91afdf38333e00a657cb476bc0030f57
dmitriy <dmitriy.malec@gmail.com>
2016-12-30 00:00:48 +0200

    - fixes in unit tests

--- android/src/test/java/com/gettipsi/nativestore/NativeStoreUnitTest.java
+++ android/src/test/java/com/gettipsi/nativestore/NativeStoreUnitTest.java
@@ -1,9 +1,12 @@
 package com.gettipsi.nativestore;
 
+import android.util.Log;
+
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.JavaOnlyArray;
 import com.facebook.react.bridge.JavaOnlyMap;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeMap;
 import com.gettipsi.nativestore.store.NativeStore;
 import com.gettipsi.nativestore.store.Observer;
 import com.gettipsi.nativestore.util.NativeObserverStub;
@@ -29,122 +32,127 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnit4.class)
-@PrepareForTest({Arguments.class})
+@PrepareForTest({Arguments.class, WritableNativeMap.class, Log.class})
 public class NativeStoreUnitTest {
 
-    @Rule
-    public final PowerMockRule rule = new PowerMockRule();
+  @Rule
+  public final PowerMockRule rule = new PowerMockRule();
 
-    private static final String REACT_OBSERVER_NAME = "REACT_OBSERVER_NAME";
-    private static final String TEST_KEY = "test_key";
-    private static final String TEST_VALUE = "test_value";
-    private NativeStore nativeStore;
+  private static final String REACT_OBSERVER_NAME = "REACT_OBSERVER_NAME";
+  private static final String TEST_KEY = "test_key";
+  private static final String TEST_VALUE = "test_value";
+  private NativeStore nativeStore;
 
-    //Without this lines I have the same problem:
-    // http://stackoverflow.com/questions/35275772/unsatisfiedlinkerror-when-unit-testing-writablenativemap
-    @Before
-    public void setUp() {
-        PowerMockito.mockStatic(Arguments.class);
-        PowerMockito.when(Arguments.createArray()).thenAnswer(new Answer<Object>() {
-            @Override
-            public Object answer(InvocationOnMock invocation) throws Throwable {
-                return new JavaOnlyArray();
-            }
-        });
-        PowerMockito.when(Arguments.createMap()).thenAnswer(new Answer<Object>() {
-            @Override
-            public Object answer(InvocationOnMock invocation) throws Throwable {
-                return new JavaOnlyMap();
-            }
-        });
-        nativeStore = NativeStore.getInstance();
+  //Without this lines I have the same problem:
+  // http://stackoverflow.com/questions/35275772/unsatisfiedlinkerror-when-unit-testing-writablenativemap
+  @Before
+  public void setUp() {
+    PowerMockito.mockStatic(Arguments.class);
+    PowerMockito.when(Arguments.createArray()).thenAnswer(new Answer<Object>() {
+      @Override
+      public Object answer(InvocationOnMock invocation) throws Throwable {
+        return new JavaOnlyArray();
+      }
+    });
+    PowerMockito.when(Arguments.createMap()).thenAnswer(new Answer<Object>() {
+      @Override
+      public Object answer(InvocationOnMock invocation) throws Throwable {
+        return new JavaOnlyMap();
+      }
+    });
+    nativeStore = NativeStore.getInstance();
+    PowerMockito.mockStatic(Log.class);
+  }
+
+  @After
+  public void clearData() {
+    NativeStore.getInstance().close();
+  }
+
+  @Test
+  public void instanceCreationTest() throws Exception {
+    assertNotNull("Store instance didn't create", NativeStore.getInstance());
+  }
+
+  @Test
+  public void setStateTest() throws Exception {
+    final Map testMap = setValue(TEST_KEY, TEST_VALUE);
+    Thread.sleep(500);
+    final Map mapFromStore = nativeStore.getState().getNativeMap();
+
+    assertEquals("Object didn't added to store", mapFromStore, testMap);
+  }
+
+  @Test
+  public void subscribeReactTest() throws Exception {
+    final ReactObserverStub observer = subscribeReact(REACT_OBSERVER_NAME);
+    changeStoreStateThreeTimes();
+    Thread.sleep(500);
+
+    assertTrue("Observer didn't send 3 events, or currentStateMap == null, or currentStateMap no instanceof WritableMap",
+      observer.getEventCounter() == 3
+        && observer.getCurrentStateMap() != null);
+  }
+
+  @Test
+  public void unsubscribeReactTest() throws Exception {
+    final ReactObserverStub observer = subscribeReact(REACT_OBSERVER_NAME);
+    unsubscribe(observer);
+    changeStoreStateThreeTimes();
+    Thread.sleep(500);
+
+    assertEquals("The observer was not unsubscribe", observer.getEventCounter(), 0);
+  }
+
+  @Test
+  public void subscribeNativeTest() throws Exception {
+    final NativeObserverStub observer = subscribeNative();
+    changeStoreStateThreeTimes();
+    Thread.sleep(500);
+
+    assertTrue("Observer didn't receive 3 updates, or currentStateMap == null, or currentStateMap no instanceof Map",
+      observer.getUpdateCounter() == 3
+        && observer.getCurrentStateMap() != null
+        && observer.getCurrentStateMap() instanceof Map);
+  }
+
+  @Test
+  public void unsubscribeNativeTest() throws Exception {
+    final NativeObserverStub observer = subscribeNative();
+    unsubscribe(observer);
+    changeStoreStateThreeTimes();
+    Thread.sleep(500);
+
+    assertEquals("The observer was not unsubscribe", observer.getUpdateCounter(), 0);
+  }
+
+  private Map<String, Object> setValue(final String key, final String value) {
+    final HashMap<String, Object> testMap = new HashMap<>();
+    testMap.put(key, value);
+    nativeStore.setState(testMap);
+    return testMap;
+  }
+
+
+  private ReactObserverStub subscribeReact(final String observerName) {
+    final ReactObserverStub observerStub = new ReactObserverStub(observerName);
+    nativeStore.subscribe(observerStub);
+    return observerStub;
+  }
+
+  private NativeObserverStub subscribeNative() {
+    final NativeObserverStub observerStub = new NativeObserverStub();
+    nativeStore.subscribe(observerStub);
+    return observerStub;
+  }
+
+  private void unsubscribe(final Observer observer) {
+    nativeStore.unsubscribe(observer);
+  }
+
+  private void changeStoreStateThreeTimes() {
+    for (int i = 0; i < 3; i++) {
+      setValue(TEST_KEY + i, TEST_VALUE + i);
     }
-
-    @After
-    public void clearData() {
-        NativeStore.getInstance().close();
-    }
-
-    @Test
-    public void instanceCreationTest() throws Exception {
-        assertNotNull("Store instance didn't create", NativeStore.getInstance());
-    }
-
-    @Test
-    public void setStateTest() throws Exception {
-        final Map testMap = setValue(TEST_KEY, TEST_VALUE);
-        final Map mapFromStore = nativeStore.getState().getNativeMap();
-
-        assertEquals("Object didn't added to store", mapFromStore, testMap);
-    }
-
-    @Test
-    public void subscribeReactTest() throws Exception {
-        final ReactObserverStub observer = subscribeReact(REACT_OBSERVER_NAME);
-        changeStoreStateThreeTimes();
-
-        assertTrue("Observer didn't send 3 events, or currentStateMap == null, or currentStateMap no instanceof WritableMap",
-                observer.getEventCounter() == 3
-                        && observer.getCurrentStateMap() != null
-                        && observer.getCurrentStateMap() instanceof WritableMap);
-    }
-
-    @Test
-    public void unsubscribeReactTest() throws Exception {
-        final ReactObserverStub observer = subscribeReact(REACT_OBSERVER_NAME);
-        unsubscribe(observer);
-        changeStoreStateThreeTimes();
-
-        assertEquals("The observer was not unsubscribe", observer.getEventCounter(), 0);
-    }
-
-    @Test
-    public void subscribeNativeTest() throws Exception {
-        final NativeObserverStub observer = subscribeNative();
-        changeStoreStateThreeTimes();
-
-        assertTrue("Observer didn't receive 3 updates, or currentStateMap == null, or currentStateMap no instanceof Map",
-                observer.getUpdateCounter() == 3
-                        && observer.getCurrentStateMap() != null
-                        && observer.getCurrentStateMap() instanceof Map);
-    }
-
-    @Test
-    public void unsubscribeNativeTest() throws Exception {
-        final NativeObserverStub observer = subscribeNative();
-        unsubscribe(observer);
-        changeStoreStateThreeTimes();
-
-        assertEquals("The observer was not unsubscribe", observer.getUpdateCounter(), 0);
-    }
-
-    private Map<String, Object> setValue(final String key, final String value) {
-        final HashMap<String, Object> testMap = new HashMap<>();
-        testMap.put(key, value);
-        nativeStore.setState(testMap);
-        return testMap;
-    }
-
-
-    private ReactObserverStub subscribeReact(final String observerName) {
-        final ReactObserverStub observerStub = new ReactObserverStub(observerName);
-        nativeStore.subscribe(observerStub);
-        return observerStub;
-    }
-
-    private NativeObserverStub subscribeNative() {
-        final NativeObserverStub observerStub = new NativeObserverStub();
-        nativeStore.subscribe(observerStub);
-        return observerStub;
-    }
-
-    private void unsubscribe(final Observer observer) {
-        nativeStore.unsubscribe(observer);
-    }
-
-    private void changeStoreStateThreeTimes() {
-        for (int i = 0; i < 3; i++) {
-            setValue(TEST_KEY + i, TEST_VALUE + i);
-        }
-    }
+  }
 }
